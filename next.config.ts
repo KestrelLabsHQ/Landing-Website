f200daa3d23cc4108d864a463b47d245055ee2ad
Daymian Tomczyk <daymian.tomczyk@outlook.com>
2026-03-18 14:52:49 -0400

    Remove Cloudflare hook from Next config

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,4 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-if (process.env.NODE_ENV === "development" && !process.env.VERCEL) {
-  void import("@opennextjs/cloudflare").then((m) => m.initOpenNextCloudflareForDev());
-}
-
 export default nextConfig;
